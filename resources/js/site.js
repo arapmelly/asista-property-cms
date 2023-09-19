b46b5a0f4747805385cae112f1c61410222d1a58
arapmelly <jacqchumo@gmail.com>
2023-09-19 21:10:59 +0000

    added gsap and barbajs

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,1 +1,19 @@
 // This is all you.
+import barba from '@barba/core';
+import { gsap } from "gsap";
+
+barba.init({
+    transitions: [{
+      name: 'opacity-transition',
+      leave(data) {
+        return gsap.to(data.current.container, {
+          opacity: 0
+        });
+      },
+      enter(data) {
+        return gsap.from(data.next.container, {
+          opacity: 0
+        });
+      }
+    }]
+  });
